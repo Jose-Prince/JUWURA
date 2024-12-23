713230c62770cd512b2b2c510a9e5b10c1b0cb94
Jose-Prince <joseprince148@gmail.com>
2024-12-22 18:48:10 -0600

    Style: initial modal for creating project

--- FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+002f74859e575bab65ee44132fee38d73b7118a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca55d7aecd332b80f559f963a2aa9395344d3628")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
