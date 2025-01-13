a1da47c54721fb50c97b55ce381eda3c3ace2bed
Andre <Andrewlol774@gmail.com>
2025-01-13 15:51:48 -0600

    adding debug

--- FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca39a6173886baee27b13da07d0c08e1adda0ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb6e0cceb60083ac7d8a80f31ef37c8584c5e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
